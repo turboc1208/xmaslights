d9f616904a3ea9d61009b651a8b0ff2244610b8e
turboc1208 <chip.cox@coxdogs.com>
2019-01-26 21:27:12 -0600

    unknown

--- XmasLights.py
+++ XmasLights.py
@@ -21,10 +21,32 @@ class XmasLights(hass.Hass):
      self.listen_state(self.turn_on_lights,"switch.front_porch_light_switch",new="on", old="off", duration=60)
 
   def turn_on_lights(self, entity,attribute,old,new, kwargs):
-    self.run_every(self.change_colors,self.datetime(),60)
+    if self.get_state("input_boolean.xmas")=="on":
+      self.xmashandle[self.run_every(self.change_colors,self.datetime(),60)]="timer"
+    else:
+      if not self.xmashandle=={}:
+        self.shutdown(self.xmashandle)
 
   def change_colors(self, kwargs):
-    for e in self.xmaslights: 
-      newcolor=self.colors[random.randint(0,len(self.colors)-1)]
-      self.log("changing lights on entity = {} to {}".format(e,newcolor))
-      self.turn_on(e,color_name=newcolor)
+    if self.get_state("input_boolean.xmas")=="on":
+      for e in self.xmaslights: 
+        newcolor=self.colors[random.randint(0,len(self.colors)-1)]
+        self.log("changing lights on entity = {} to {}".format(e,newcolor))
+        self.turn_on(e,color_name=newcolor)
+    else:
+      self.shutdown(self.xmashandle)
+
+  def shutdown(self, hdict):
+    self.log("hdict={}".format(hdict))
+    for h in hdict:
+      if hdict[h]=="event":
+        self.log("Shutting down event {}".format(h))
+        self.cancel_event(h)
+      elif hdict[h]=="timer":
+        self.log("Shutting down timer {}".format(h))
+        self.cancel_timer(h)
+      else:
+        self.log("unknown handle type {} for handle {}".format(hdict[h],h))
+    hdict={}
+    for e in self.xmaslights:
+      self.turn_on(e,color_name="White")
